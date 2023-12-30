998a2e503aafdec9caa031a3c4529041423b91fb
Trym Thorén <69774200+trymthoren@users.noreply.github.com>
2023-12-30 02:30:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 # Not 100% satisfied with the code yet, as its to clunky so im trimming it to do the same job, but with less code. Will be posted once ready
+# Kivy + kivymd or pyqt5 will be used for the GUI as its much more cross-platform friendly.
+# Might make this as a both Linux (deb) and Windows installable (exe).
